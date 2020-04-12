808b0148170cf81b24f97c2d8ad5278886ecced5
towzeur <towzeur@gmail.com>
2020-04-12 21:01:22 +0200

    moved is_marbles_clicked to gameutils

--- gameutils.py
+++ gameutils.py
@@ -1,5 +1,6 @@
 import json
 import random
+import pyglet
 
 class AbaloneUtils:
 
@@ -23,3 +24,56 @@ class AbaloneUtils:
     @staticmethod
     def get_variants(variant_name='classical', random_pick=False):
         return AbaloneUtils._safe_json_pick(variant_name, 'assets/variants.json', 'classical', random_pick)
+    
+    @staticmethod
+    def get_im_centered(im_path):
+        im = pyglet.image.load(im_path)
+        im.anchor_x = im.width  // 2
+        im.anchor_y = im.height // 2
+        return im
+
+    @staticmethod
+    def is_marbles_clicked(x, y, theme):
+        """
+        return the pos of the clicked marble for a given theme.
+        
+        It do so by starting in the first row and increse the col.
+        It's a bit better than bruteforce because it skips the current row if the 
+        first elt (first column) is not within the outter box.
+        if the click is find inside a box, it compute the L2 norm to find
+        if the click is inded upon the marble.
+
+        Returns:
+            (int) pos : pos of the clicked marble if it exists (0 < pos < self.theme['locations'] )
+            (int)  -1 : otherwise
+        """
+
+        R = theme['dimension']['marble_radius']
+
+        pos = 0
+        for row, nb_col in enumerate(theme['rows']):
+            for col in range(nb_col):
+                x_c, y_c = theme['coordinates'][pos]
+                # we first start by checking if the click is in the circle's outter box
+                # are we in the good row ?
+                if abs(y -  y_c) <= R:
+                    # then if a solution exist it may be on this row
+                    # is it on this box ?
+                    if abs(x -  x_c) <= R:
+                        # is it on the circle or somwhere else inside the box ?
+                        # if so we have a winner
+                        if (x-x_c)**2 + (y-y_c)**2 < R**2:
+                            return pos
+                        # otherwise, because the click is within in the right box
+                        # it can't be on another box
+                        else:
+                            return -1
+                    else:
+                        pos += 1
+                    # then maybe it is on another box in the same row
+                    # (the next iner loop iteration)
+                # try on the next row
+                else:
+                    pos +=  theme['rows'][row]
+                    break
+        return -1
