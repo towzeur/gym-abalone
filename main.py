3243cee44f69c66fbbc3c3096918bc42687cdbf6
towzeur <towzeur@gmail.com>
2020-04-12 05:57:58 +0200

    extraction of marble gui logic to a dedicated class and added selected overlay.

--- main.py
+++ main.py
@@ -8,11 +8,71 @@ import json
 from gamelogic import AbaloneGame
 from gameutils import AbaloneUtils
 
+
+class Marble:
+    
+    LABEL_COLORS = {
+        'default' : (0, 0, 0, 255),
+        1  : (0, 0, 0, 255),
+        2  : (255, 255, 255, 255)
+    }
+    
+    def __init__(self, player, theme, batch, groups, debug=True):
+        self.player = player
+        self.player_token = player + 1
+
+        self.theme = theme
+        self.batch = batch
+        self.groups = groups
+        self.debug = debug
+
+        self.sprite = None
+        self.label = None
+        self._init_sprites()
+
+        self.pos = None
+
+    def _init_sprites(self):
+        # marble sprite
+        im_path = self.theme['sprites']['players'][self.player]
+        im = pyglet.image.load(im_path)
+        self.sprite = pyglet.sprite.Sprite(im, batch=self.batch, group=self.groups[1])
+        # label sprite if debug
+        if self.debug:
+            color = Marble.LABEL_COLORS.get(self.player_token, Marble.LABEL_COLORS['default'])
+            self.label = pyglet.text.Label(
+                font_name='Roboto', font_size=28,
+                color = color,
+                anchor_x='center', anchor_y='center',
+                batch=self.batch, group=self.groups[2]
+            )
+
+    def delete(self):
+        if self.sprite:
+            self.sprite.delete()
+            self.sprite = None
+        if self.label:
+            self.label.delete()
+            self.label = None
+
+    def change_position(self, pos):
+        if self.pos != pos:
+            self.pos = pos
+
+            x_new, y_new = self.theme['coordinates'][pos]
+            self.sprite.update(x_new, y_new)
+
+            if self.debug:
+                self.label.x = x_new + self.theme['dimension']['marble_radius']
+                self.label.y = y_new + self.theme['dimension']['marble_radius']
+                self.label.text = str(pos)
+
+
 class window(pyglet.window.Window):
 
     def __init__(self, theme="default"):
 
-        # set the theme
+        # set the theme 
         self.theme = AbaloneUtils.get_theme(theme)
 
         # init the game engine
@@ -46,90 +106,71 @@ class window(pyglet.window.Window):
         board_image = pyglet.image.load(self.theme['sprites']['board'])
         self.board_sprite = pyglet.sprite.Sprite(board_image, batch=self.batch, group=self.groups[0])
         
-        self.players_cells = None
+        # selected sprite
+        selected_sprite = pyglet.image.load(self.theme['sprites']['selected'])
+        self.selected_sprite = pyglet.sprite.Sprite(selected_sprite, batch=self.batch, group=self.groups[2])
+        self.selected_sprite.visible = False
+
+        self.marbles = None
+
+    def init_window(self, variant_name='classical', random_pick=True, debug=True):
+        # reset the game
+        self.game.init_game(variant_name=variant_name, random_pick=random_pick)
+
+        # reset players's sprites
+        if self.marbles:
+            for marble in self.marbles:
+                if marble:
+                    marble.delete()
+                    del marble
+
+        # init marbles
+        self.marbles = [None] * self.theme['locations']
+        for player in range(self.game.players):
+            for pos in self.game.players_sets[player]:
+                marble = Marble(player, self.theme, self.batch, self.groups, debug=debug)
+                marble.change_position(pos)
+                self.marbles[pos] = marble
 
     def on_draw(self):
         self.clear()
         self.batch.draw()
 
-    def init_window(self, variant_name='classical', random_pick=True, debug=True):
-
-        # reset the game
-        self.game.init_game(variant_name=variant_name, random_pick=random_pick)
-
-        # reset players's sprites
-        if self.players_cells:
-            for player_cells in self.players_cells:
-                for cell in player_cells:
-                    cell['sprite'].delete()
-                    if 'pos_text' in cell:
-                        cell['pos_text'].delete()
-                    del cell
-        self.players_cells = None
-
-        # draw the board for the first time
-        self.draw_board(debug=debug)
-
-    def draw_token(self, pos, player=1, debug=True):
-        cell = {
-            'pos':pos,
-            'out':False
-        }
-
-        # compute the coords from the pos
-        x, y = self.theme['coordinates'][pos]
-
-        # select the right player image
-        im_path = self.theme['sprites']['players'][player-1]
-
-        # create the sprite
-        im = pyglet.image.load(im_path)
-        cell['sprite'] = pyglet.sprite.Sprite(im, batch=self.batch, group=self.groups[1], x=x, y=y)
-
-        if debug:
-            cell['pos_text'] = pyglet.text.Label(
-                f'{pos}', x=x, y=y,
-                font_name='Roboto',
-                font_size=28,
-                color=(0, 0, 0, 255) if player==1 else (255, 255, 255, 255),
-                anchor_x='left', anchor_y='bottom',
-                batch=self.batch,
-                group=self.groups[2]
-            )
-
-        return cell
-
-    def draw_board(self, debug=True):
-        self.players_cells = [[] for p in range(self.game.players)]
-        for p in range(self.game.players):
-            TOKEN_PLAYER = p + 1
-            for pos in self.game.players_sets[p]:
-                cell = self.draw_token(pos, player=TOKEN_PLAYER, debug=debug)
-                self.players_cells[p].append(cell)
-
     def update(self, dt):
-        self.init_window(debug=False)
-        return 
+        self.init_window(random_pick=True, debug=True)
+
 
     def on_mouse_press(self, x, y, button, modifiers):
         print(f'({x}, {y})')
         pos = self.is_marbles_clicked(x, y)
+
         if pos != -1:
-            player_token = self.game.action(pos)
-            player = player_token - 1
-            # find the cell
-            for cell in self.players_cells[player]:
-                if cell['pos'] == pos:
-                    damage_index = self.game.players_damages[player]
-                    new_x, new_y = self.theme['out_coordinates'][player][damage_index]
-                    cell['sprite'].update(x=new_x, y=new_y)
-                    # increase damage
-                    self.game.players_damages[player] += 1
-                    break
-            
 
+            token = self.game.action(pos)
+           
+            if token != self.game.TOKEN_EMPTY:
 
+                # show the slected border
+                x, y = self.theme['coordinates'][pos]
+                offset = (self.selected_sprite.width - 48)/2
+                self.selected_sprite.visible = True
+                self.selected_sprite.update(x=x-offset, y=y-offset)
 
+                #player = token - 1
+                # find the cell
+                #for cell in self.players_cells[player]:
+                #    if cell['pos'] == pos:
+                #        pass
+                        
+                '''
+                damage_index = self.game.players_damages[player]
+                new_x, new_y = self.theme['out_coordinates'][player][damage_index]
+                cell['sprite'].update(x=new_x, y=new_y)
+                # increase damage
+                self.game.players_damages[player] += 1
+                break
+                '''
+                    
 
     def is_marbles_clicked(self, x, y):
         """
@@ -154,21 +195,19 @@ class window(pyglet.window.Window):
             for col in range(nb_col):
                 x_bot_left, y_bot_left = self.theme['coordinates'][pos]
                 # we first start by checking if the click is in the circle's outter box
-                # are we in the right row ?
+                # are we in the good row ?
                 if 0 <= (y -  y_bot_left) <= C:
                     # then if a solution exist it may be on this row
                     # is it on this box ?
                     if 0 <= (x -  x_bot_left) <= C:
-                        
                         x_c = x_bot_left + R
                         y_c = y_bot_left + R
                         # is it on the circle or somhere else inside the box ?
                         # if so we have a winner
                         if (x-x_c)**2 + (y-y_c)**2 < R**2:
                             return pos
-                        # otherwise, because we are in the right box
-                        # we can't be on another box so there is no hope 
-                        # for finding a clicled marble
+                        # otherwise, because the click is within in the right box
+                        # it can't be on another box
                         else:
                             return -1
                     else:
