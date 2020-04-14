e729cbae3edf0f94bad0f9ad973fa6ab8d3b1bda
towzeur <towzeur@gmail.com>
2020-04-14 03:15:46 +0200

    coded action handler et dispatcher : it return a dic with modification that is used to update the GUI. Basic deplacement and turn is now working. Deleted player token => now token starts at 0 : there is no distinction between #player and player token.

--- gamelogic.py
+++ gamelogic.py
@@ -1,6 +1,6 @@
 import numpy as np
 from gameutils import AbaloneUtils
-
+ 
 class AbaloneGame:
 
     # Actions : possible 
@@ -11,14 +11,19 @@ class AbaloneGame:
     DOWN_LEFT  = (+1, -1)
     DOWN_RIGHT = (+1,  0)
 
-    ACTIONS = [LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT]
+    #   UP_LEFT : 4     5 : UP_RIGHT
+    #              \   /
+    #               \ /
+    # LEFT : 3 ----- * ----- 0 : RIGHT 
+    #               /  \ 
+    #              /    \ 
+    # DOWN_LEFT : 2      1 : DOWN_RIGHT
+
+    ACTIONS = [RIGHT, DOWN_RIGHT, DOWN_LEFT, LEFT, UP_LEFT, UP_RIGHT]
 
     # tokens
-    TOKEN_VOID     = -1
-    TOKEN_EMPTY    =  0
-    # TOKEN_PLAYER_1 =  1 
-    # TOKEN_PLAYER_2 =  2
-    # the player # n have token n
+    TOKEN_VOID     = -2
+    TOKEN_EMPTY    = -1
 
     BOARD_SIZE = 11
 
@@ -31,11 +36,14 @@ class AbaloneGame:
         # var for the game logic
         self.players_sets = []
         self.players_damaged_sets = []
-        self.current_player = None
-        self.turns_count = 0
-        #self.
 
-    def init_game(self, variant_name='classical', random_pick=False):
+        # current
+        self.turns_count = 0
+        self.current_player = None
+        self.current_pos = None
+        
+
+    def init_game(self, player=0, random_player=True, variant_name='classical', random_pick=False):
 
         self.board = self.new_board()
 
@@ -57,12 +65,16 @@ class AbaloneGame:
 
         # fill the board
         for p in range(self.players):
-            TOKEN_PLAYER = p + 1
             for pos in self.players_sets[p]:
                 r, c = self.positions[pos]
-                self.board[r, c] = TOKEN_PLAYER
-
+                self.board[r, c] = p
         self.players_damages = [0 for p in range(self.players)]
+
+        #
+        self.current_pos = None
+        self.current_player = np.random.randint(self.players) if random_player else player
+        self.turns_count = 0
+
         
     @staticmethod
     def new_board():
@@ -109,11 +121,96 @@ class AbaloneGame:
         n = AbaloneGame.BOARD_SIZE
         return [(r, c) for r in range(n) for c in range(n) if board[r, c] == token]
 
-    def action(self, clicked_pos):
-        clicked_index = self.positions[clicked_pos]
-        clicked_token = self.board[clicked_index]
-        print(clicked_pos, clicked_token)
-        return clicked_token
+    def get_coords_from_pos(self, pos):
+        r, c = self.positions[pos]
+        return r, c
+
+    def get_pos_from_coords(self, r, c):
+        pos = self.positions.index((r, c))
+        return pos
+
+    def get_token_from_pos(self, pos):
+        r, c = self.get_coords_from_pos(pos)
+        token = self.board[r, c]
+        return token
+
+    def get_neighbors(self, r, c):
+        return [(r+dr, c+dc) for (dr, dc) in AbaloneGame.ACTIONS]
+
+    def get_neighbors_grouped(self, r, c):
+        neighbors_grouped = {}
+        for (neighbor_r, neighbor_c) in self.get_neighbors(r, c):
+            neighbor_token = self.board[neighbor_r, neighbor_c]
+            if neighbor_token not in neighbors_grouped:
+                neighbors_grouped[neighbor_token] = []
+            neighbors_grouped[neighbor_token].append((neighbor_r, neighbor_c))
+        return neighbors_grouped
+
+    def next_turn(self):
+        self.current_pos = None
+        self.current_player = (self.current_player + 1) % self.players
+        self.turns_count += 1
+
+    def _action_change_current_pos(self, pos):
+        print('_action_change_current_pos')
+        out = {'selected' : (self.current_pos, pos)}
+        self.current_pos = pos
+        return out
+
+    def _action_move(self, r, c):
+        print('_action_move')
+        out = {}
+
+        r_curr, c_curr = self.positions[self.current_pos]
+        neighbors_curr = self.get_neighbors(r_curr, c_curr)
+        
+        # print('*', (r_curr, c_curr), (r,c), neighbors_curr)
+        # check for single move:
+        if (r, c) in neighbors_curr:
+            # swap
+            self.board[r, c], self.board[r_curr, c_curr] = self.board[r_curr, c_curr], self.board[r, c]
+            out['moves'] = [(
+                self.get_pos_from_coords(r_curr, c_curr),       # old_pos
+                self.get_pos_from_coords(r, c),                 # new pos
+                AbaloneGame.ACTIONS.index((r-r_curr, c-c_curr)) # angle direction
+            )]
+            out['new_turn'] = self.current_pos
+            self.next_turn()
+
+        # check for 2 move
+        elif False:
+            pass
+
+        # check for 3 move
+        elif False:
+            pass
+
+        return out
+
+    def action_handler(self, pos):
+        r, c = self.get_coords_from_pos(pos)
+        token = self.board[r, c]
+
+        print(token, self.current_pos, self.current_player)
+
+        # if we clicked on a empty position
+        if token == AbaloneGame.TOKEN_EMPTY:
+            # is this a missclick ?
+            if self.current_pos is None:
+                return
+            # can the current_pos be moved to the new pos ?
+            else:
+                return self._action_move(r, c)
+
+        # we clicked on a player
+        else:
+            # is this the current player ?
+            if token == self.current_player:
+                return self._action_change_current_pos(pos)
+            # this a another player position
+            else:
+                pass
+
 
 
 if __name__ == '__main__':
